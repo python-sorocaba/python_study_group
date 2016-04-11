5bcbb0f72c0af16626b36b3d4740e811453a8c6c
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-04-10 22:39:17 -0300

    Create our basic test structure

---
+++ blackjack/rafaelhenrique/tests.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestBlackjack(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_problem(self):
+        self.assertEqual(True, False)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

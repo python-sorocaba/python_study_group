ac0d550afc82ea49e1bb8a5932285c157606b766
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-05-23 23:31:27 -0300

    Add pattern Fluent Interface

---
+++ examples/patterns/fluent_interface.py
@@ -0,0 +1,16 @@
+class Poem(object):
+    def __init__(self, content):
+        self.content = content
+
+    def indent(self, spaces):
+        self.content = " " * spaces + self.content
+        return self
+
+    def suffix(self, content):
+        self.content = self.content + " - " + content
+        return self
+
+
+if __name__ == "__main__":
+    poem = Poem("Road Not Travelled").indent(4).suffix("Robert Frost").content
+    print(poem)

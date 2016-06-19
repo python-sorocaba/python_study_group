241459679755da89484c97f9fc8035bcbde0c06d
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-06-19 20:54:55 -0300

    Fix tests

--- blackjack_oo/rafaelhenrique/tests/test_player.py
+++ blackjack_oo/rafaelhenrique/tests/test_player.py
@@ -7,7 +7,7 @@ class TestPlayer(unittest.TestCase):
 
     def setUp(self):
         self.player = Player('Rafael', 2000.0)
-        self.card_one = Card('11', 'spades')
+        self.card_one = Card('Q', 'spades')
         self.card_two = Card('A', 'spades')
 
     def test_repr(self):
@@ -17,16 +17,16 @@ class TestPlayer(unittest.TestCase):
 
     def test_points(self):
         self.player.hand.append(self.card_one)
-        self.assertEqual(self.player.points, 11)
+        self.assertEqual(self.player.points, 10)
 
     def test_one_card_on_hand_append(self):
         self.player.hand.append(self.card_one)
-        self.assertEqual(str(self.player.hand), "1 card: 11spades")
+        self.assertEqual(str(self.player.hand), "1 card: Q spades")
 
     def test_two_cards_on_hand_append(self):
         self.player.hand.append(self.card_one)
         self.player.hand.append(self.card_two)
-        self.assertEqual(str(self.player.hand), "2 cards: 11spades,Aspades")
+        self.assertEqual(str(self.player.hand), "2 cards: Q spades,A spades")
 
     def test_show_money(self):
         self.assertEqual(str(self.player.money), "2000")

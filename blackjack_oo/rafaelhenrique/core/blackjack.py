5756389c080796b751921afb6e6a07993c681005
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-06-19 22:32:05 -0300

    Add new Blackjack class

--- blackjack_oo/rafaelhenrique/core/blackjack.py
+++ blackjack_oo/rafaelhenrique/core/blackjack.py
@@ -1,28 +1,37 @@
-# -*- coding: utf-8 -*-
-from decimal import Decimal
+"""
+Doctest for Blackjack
+
+>>> from core.frenchdeck import FrenchDeck
+>>> from core.blackjack import Blackjack
+>>> from core.player import Player
+>>> deck = FrenchDeck()
+>>> player = Player("Rafael", 2000.0)
+>>> dealer = Player("Dealer", 0)
+>>> game = Blackjack(dealer, player, deck)
+>>> game.start()
+>>> len(dealer.hand) == 2
+True
+"""
+
+from random import shuffle
 
 
 class Blackjack:
-    numbers = ["A", "2", "3", "4", "5", "6",
-               "7", "8", "9", "10", "Q", "J",
-               "K"]
-    suits = ["♣", "♦", "♥", "♠"]
+    def __init__(self, dealer, player, deck):
+        self.dealer = dealer
+        self.player = player
+        self.deck = deck
+        self.started = False
 
-    def __init__(self):
-        self.deck = ["{}{}".format(number, suit)
-                     for suit in self.suits
-                     for number in self.numbers]
-        self.money = Decimal('2000.0')
+    def __repr__(self):
+        return "Blackjack(dealer={!r}, player={!r})".format(self.dealer.name,
+                                                            self.player.name)
 
-    def bet(self, coin):
-        if coin not in (1, 5, 25, 100):
-            raise Exception("Invalid coin to bet")
-        if self.money >= coin:
-            self.money -= coin
-            return coin
-        return None
-
-    def surrender(self):
-        print("Finish with...")
-        self.show_points()
-        raise SystemExit
+    def start(self):
+        if self.started:
+            return
+        shuffle(self.deck)
+        for _ in range(2):
+            card = self.deck.pop(0)
+            self.dealer.hand.append(card)
+        self.started = True

2893735eca972b88c43a44aa48ea96b7dfaf9a1e
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-06-19 22:34:23 -0300

    Create new play

--- blackjack_oo/rafaelhenrique/play.py
+++ blackjack_oo/rafaelhenrique/play.py
@@ -1,8 +1,10 @@
 from core.frenchdeck import FrenchDeck
+from core.blackjack import Blackjack
 from core.player import Player
 
 deck = FrenchDeck()
-player = Player("Rafael")
-card = deck.pop(0)
-player.hand.append(card)
-print(player.points)
+player = Player("Rafael", 2000.0)
+dealer = Player("Dealer", 0)
+game = Blackjack(dealer, player, deck)
+game.start()
+print(len(dealer.hand) == 2)

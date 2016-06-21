1565e25ebac43ffcf0059d26f4a0730a20b95d4d
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-06-20 22:59:01 -0300

    Improve logger decorator

--- examples/decorator/logger.py
+++ examples/decorator/logger.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 
 def logger(func):
-    def func_wrapper(x, y):
-        return_func = func(x, y)
+    def func_wrapper(*args, **kwargs):
+        return_func = func(*args, **kwargs)
         date = datetime.now().strftime("%d-%m-%Y %T")
         msg = "{0} -> func_name {1}: Return {2}\n".format(date, func.__name__,
                                                           return_func)
@@ -23,6 +23,12 @@ def sub(x, y):
     return x - y
 
 
+@logger
+def full_name(first_name, middle_name, last_name):
+    return "{} {} {}".format(first_name, middle_name, last_name)
+
+
 if __name__ == "__main__":
     print(sum(1, 2))
     print(sub(2, 1))
+    print(full_name("Rafael", "Henrique", "Correia"))

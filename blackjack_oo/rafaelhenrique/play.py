4d6041bb4aae49c4ff843a00099c1e43d6ad1912
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-06-26 22:01:59 -0300

    Improve play

--- blackjack_oo/rafaelhenrique/play.py
+++ blackjack_oo/rafaelhenrique/play.py
@@ -1,10 +1,27 @@
+import os
+
 from core.frenchdeck import FrenchDeck
 from core.blackjack import Blackjack
 from core.player import Player
 
-deck = FrenchDeck()
-player = Player("Rafael", 2000.0)
-dealer = Player("Dealer", 0)
-game = Blackjack(dealer, player, deck)
-game.start()
-print(len(dealer.hand) == 2)
+
+def menu():
+    """Clear screen and create option menu"""
+    msg = ("Jogar: O que gostaria de fazer?\n"
+           "\t1. Apostar\n"
+           "\t2. Comprar carta\n"
+           "\t3. Dobrar aposta\n"
+           "\t4. Parar/Finalizar jogada\n"
+           "\t5. Chorar pra mãe\n"
+           "Digite número desejado: ")
+    return msg
+
+if __name__ == "__main__":
+    deck = FrenchDeck()
+    player = Player("Rafael", 2000.0)
+    dealer = Player("Dealer", 0)
+    game = Blackjack(dealer, player, deck)
+    game.start()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(game.header)
+    print(menu())
